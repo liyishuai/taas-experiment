2bcf8d702ba9ada912c0adf1e79871131317285f
Yishuai Li <liyishuai.lys@alibaba-inc.com>
2023-10-16 15:16:34 +0800

    refine jitter figure

--- tools/pd-tso-bench/jitter.py
+++ tools/pd-tso-bench/jitter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 taas_edge_color = 'xkcd:violet'
 tso_edge_color = 'xkcd:rust'
@@ -8,17 +9,41 @@ tso_color = 'white'
 
 plt.rc('font', size=16)
 plt.margins(0)
-taas0_freq = [16264, 2444, 65, 32, 12, 7, 5, 1, 1]
-taas0_x = np.arange(0.2, 1.01, 0.1)
-taas0_pdf = [x / 2010.2 for x in taas0_freq]
 
-taas1_freq = [19729, 151, 29, 35, 20, 13, 4, 2, 1, 2, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-taas1_x = np.arange(0.3, 5.51, 0.1)
-taas1_pdf = [x / 2001.3 for x in taas1_freq]
+width = 0.006
+x012 = np.arange(0, 6+width, width)
 
-taas2_freq = [19377, 425, 85, 80, 33, 20, 11, 10, 7, 8, 7, 5, 2, 1, 2, 1, 1, 3, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1]
-taas2_x = np.arange(0.3, 4.21, 0.1)
-taas2_pdf = [x / 2008.7 for x in taas2_freq]
+taas0_freq = [0 for _ in x012]
+taas1_freq = [0 for _ in x012]
+taas2_freq = [0 for _ in x012]
+
+n0 = 0
+n1 = 0
+n2 = 0
+
+with open('taas0.csv', encoding='utf-8-sig') as file0:
+    taas0_data = csv.reader(file0)
+    for row in taas0_data:
+        n0 = n0 + 1
+        index = int(float(row[0]) / 1000 / width)
+        taas0_freq[index] = taas0_freq[index] + 1
+taas0_pdf = [x / width / n0 for x in taas0_freq]
+
+with open('taas1.csv', encoding='utf-8-sig') as file1:
+    taas1_data = csv.reader(file1)
+    for row in taas1_data:
+        n1 = n1 + 1
+        index = int(float(row[0]) / 1000 / width)
+        taas1_freq[index] = taas1_freq[index] + 1
+taas1_pdf = [x / width / n1 for x in taas1_freq]
+
+with open('taas2.csv', encoding='utf-8-sig') as file2:
+    taas2_data = csv.reader(file2)
+    for row in taas2_data:
+        n2 = n2 + 1
+        index = int(float(row[0]) / 1000 / width)
+        taas2_freq[index] = taas2_freq[index] + 1
+taas2_pdf = [x / width / n2 for x in taas2_freq]
 
 taas3_freq = [0, 0, 0, 9, 44, 108, 191, 250, 366, 498, 569, 662, 732, 826, 892, 988, 932, 1014, 990, 1017, 939, 925, 900, 863, 820, 682, 607, 612, 554, 430, 409, 342, 307, 265, 236, 207, 153, 132, 100, 89, 85, 69, 40, 32, 33, 25, 19, 10, 11, 6, 3, 8, 3, 1, 2, 2, 0, 0, 1, 0, 0]
 taas3_pdf = [x / 2001.0 for x in taas3_freq]
@@ -52,9 +77,9 @@ tidb_ipdf = [x / 2000.7 for x in tidb_interrupt]
 # tidb_p50  = [0.1456,0.1458,1.6446,1.6402]
 # tidb_p98  = [0.1635,0.1629,4.1534,4.1472]
 
-fig, (taas,tidb) = plt.subplots(1, 2, figsize=(9, 4),sharex=True,sharey=True,constrained_layout=True)
+fig, (taas012, taas345,tidb) = plt.subplots(1, 3, figsize=(12, 4),sharex=False,sharey=False,constrained_layout=True)
 
-taas.set_title('TaaS N=5',loc='left')
+taas012.set_title('TaaS N=5',loc='left')
 # taas.set_xticks(taas_x)
 # taas.set_xlabel('# Servers Interfered')
 
@@ -83,30 +108,56 @@ tidb.set_title('TiDB-PD N=5',loc='left')
 #tidb.set_yticklabels(['' for _ in latency_ticks])
 # taas.set_ylabel('Latency (ms)')
 
-taas.set_ylim(0,1.1)
-taas.set_yticks(np.arange(0, 1.1, 0.2))
-taas.set_xlim(0,6)
-taas.set_xticks([0,1,2,3,4,5,6])
-taas.set_ylabel('Density ($\mathrm{ms}^{-1}$)')
 
-taas.plot(taas0_x, taas0_pdf, ':g', label='0/5')
-taas.plot(taas1_x, taas1_pdf, dashes=[5, 1, 1, 1, 1, 1, 1, 1],color='y', label='1/5')
-taas.plot(taas2_x, taas2_pdf, dashes=[5, 1, 1, 1, 1, 1], color='c', label='2/5')
-taas.plot(x_ticks, taas3_pdf, '-.b', label='3/5')
-taas.plot(x_ticks, taas4_pdf, '--m', label='4/5')
-taas.plot(x_ticks, taas5_pdf, '-r', label='5/5')
-taas.legend(bbox_to_anchor=(.7, 1.1))
+taas012.set_ylabel('Density ($\mathrm{ms}^{-1}$)')
 
+upperi = 0.43
+upper012 = int(upperi/width)+1
+
+taas012.plot(x012[:upper012], taas0_pdf[:upper012], ':g', label='all intact')
+taas012.plot(x012[:upper012], taas1_pdf[:upper012], dashes=[5, 1, 1, 1, 1, 1, 1, 1],color='y', label='1/5 interfered')
+taas012.plot(x012[:upper012], taas2_pdf[:upper012], dashes=[5, 2, 1, 2, 1, 2], color='c', label='2/5 interfered')
+
+taas012.set_ylim(0, 22)
+taas012.set_xlim(0, upperi)
+taas012.set_xticks(np.arange(0, upperi+width, 0.1))
+taas012.legend()
+
+taas345.set_ylim(0,1.1)
+taas345.set_yticks(np.arange(0, 1.1, 0.2))
+taas345.set_xlim(0,6)
+taas345.set_xticks([0,1,2,3,4,5,6])
+
+lower0 = int(0.2/width)
+lower12 = int(0.35/width)
+upper345 = int(1/width)
+
+# taas345.plot(x012[lower0:], taas0_pdf[lower0:], ':g')
+# taas345.plot(x012[lower12:], taas1_pdf[lower12:], dashes=[5, 1, 1, 1, 1, 1, 1, 1],color='y')
+taas345.plot(x012[lower12:], taas2_pdf[lower12:], dashes=[5, 2, 1, 2, 1, 2], color='c')
+taas345.plot(x_ticks, taas3_pdf, '-.b', label='3/5 interfered')
+taas345.plot(x_ticks, taas4_pdf, '--m', label='4/5 interfered')
+taas345.plot(x_ticks, taas5_pdf, '-r', label='all interfered')
+taas345.legend()
+
+ping_lower=3
+
+tidb.set_ylim(0,1.1)
+tidb.set_yticks(np.arange(0, 1.1, 0.2))
+tidb.set_xlim(0,6)
+tidb.set_xticks([0,1,2,3,4,5,6])
 tidb.plot(tidb_x, tidb_pdf, '-.g', label='leader intact')
 tidb.plot(x_ticks, tidb_ipdf, '-r', label='leader interfered')
-tidb.plot(ping_x, ping_pdf, ':k',label='interfered ping')
+tidb.plot(ping_x[ping_lower:], ping_pdf[ping_lower:], ':k',label='interfered ping')
 tidb.legend()
 
 # lgd = fig.legend(handles=[box_taas,error_taas],ncol=2,bbox_to_anchor=(.78,1.15))
 
-taas.spines['top'].set_visible(False)
+taas012.spines['top'].set_visible(False)
+taas345.spines['top'].set_visible(False)
 tidb.spines['top'].set_visible(False)
-taas.spines['right'].set_visible(False)
+taas012.spines['right'].set_visible(False)
+taas345.spines['right'].set_visible(False)
 tidb.spines['right'].set_visible(False)
 # ax.spines['right'].set_visible(False)
 # ax.spines['left'].set_linewidth(2)
